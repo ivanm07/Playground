4bbddd8ee5367a11d97d1911a17d102cf060f506
Iván E. Mireles <ivan.mireles@carlisleit.com>
2020-02-03 16:26:59 -0700

    preview pdfs

---
+++ Playground.Forms/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Playground.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 12);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Open PDF";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // previewBox
+            // 
+            this.previewBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewBox.Location = new System.Drawing.Point(12, 41);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(776, 397);
+            this.previewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.previewBox.TabIndex = 1;
+            this.previewBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Playground";
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.PictureBox previewBox;
+    }
+}
+
---
+++ Playground.Forms/Form1.cs
@@ -0,0 +1,74 @@
+﻿
+using ImageMagick;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playground.Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MagickNET.SetGhostscriptDirectory(@"gs\");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (dialog.CheckFileExists)
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    previewBox.Image = Image.FromFile("giphy.gif");
+                    Task.Run(async () =>
+                    {
+                        MagickReadSettings settings = new MagickReadSettings
+                        {
+                            // Settings the density to 300 dpi will create an image with a better quality
+                            //Density = new Density(96)
+                        };
+
+                        using (MagickImageCollection images = new MagickImageCollection())
+                        {
+                            // Add all the pages of the pdf file to the collection
+                            images.Read(dialog.OpenFile(), settings);
+                            for (int i = 1; i <= images.Count; i++)
+                            {
+                                //foreach (MagickImage image in images)
+                                //{
+                                //    using (var stream = new MemoryStream())
+                                //    {
+                                //        previewBox.Image = image.ToBitmap();
+                                //    }
+                                //}
+                                var image = images.AppendHorizontally();
+                                using (var stream = new MemoryStream())
+                                {
+                                    previewBox.Image = image.ToBitmap();
+                                }
+                            }
+                        }
+                    }).ContinueWith(new Action<Task>((t) =>
+                    {
+                        Console.WriteLine(sw.Elapsed);
+                    }));
+                }
+            }
+        }
+    }
+}
