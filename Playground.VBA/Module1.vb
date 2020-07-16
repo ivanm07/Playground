81c19cb23103da2f418bcdd337f7ac73644d0a2c
Iván E. Mireles <ivan.mireles@carlisleit.com>
2020-07-16 09:05:54 -0700

    vb

---
+++ Playground.VBA/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports ADODB
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello home!")
+        Console.ReadKey()
+
+        'Dim Cn As Connection = New Connection()
+        'Cn.Open("")
+        'Dim Cm As Command
+        'Set Cm = New Command()
+
+    End Sub
+
+End Module
